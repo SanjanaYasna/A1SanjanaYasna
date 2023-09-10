4dce691757ed442030db7c6a5c0d6e6428df5318
Sanjana Yasna <sanjanayasna@Anans-MacBook-Pro.local>
2023-09-10 12:16:25 -0400

    first

--- app/src/main/java/practice/App.java
+++ app/src/main/java/practice/App.java
@@ -4,8 +4,20 @@
 package practice;
 
 public class App {
+    private String greeting =  "What's up!";
+    private String animal;
     public String getGreeting() {
-        return "Hello World!";
+        return greeting;
+    }
+    public void setGreeting(String newGreeting) {
+         greeting = newGreeting;
+    }
+
+    public void catsVsDogsAsk(String animal){
+        this.animal = animal;
+    }
+    public String getAnimal(){
+        return animal;
     }
 
     public static void main(String[] args) {
